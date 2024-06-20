720e2b39a649a366e0cbdad4d377b22bb4317e79
Hayden D’Souza <haydenfds@gmail.com>
2024-06-20 15:50:56 -0700

    Adding basic password modal w/ styling

--- main.ts
+++ main.ts
@@ -1,134 +1,64 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Notice, Plugin } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+class PasswordModal extends Modal {
+    private onSubmit: (password: string) => void;
 
-interface MyPluginSettings {
-	mySetting: string;
+    constructor(app: App, onSubmit: (password: string) => void) {
+        super(app);
+        this.onSubmit = onSubmit;
+    }
+
+    onOpen() {
+        const { contentEl } = this;
+        contentEl.addClass('password-modal');
+
+        contentEl.createEl('h2', { text: 'Enter Password' });
+
+        const inputEl = contentEl.createEl('input', {
+            type: 'password',
+            placeholder: 'Enter your password',
+        });
+
+        const buttonContainer = contentEl.createDiv({ cls: 'button-container' });
+        const submitButton = buttonContainer.createEl('button', { text: 'Submit' });
+
+        submitButton.onclick = () => {
+            this.onSubmit(inputEl.value);
+            this.close();
+        };
+
+        inputEl.onkeydown = (event) => {
+            if (event.key === 'Enter') {
+                this.onSubmit(inputEl.value);
+                this.close();
+            }
+        };
+    }
+
+    onClose() {
+        const { contentEl } = this;
+        contentEl.empty();
+    }
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export default class PasswordProtectPlugin extends Plugin {
+    async onload() {
+        console.log('Loading Password Protect Plugin');
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+        // Add a ribbon icon to the left sidebar
+        const ribbonIconEl = this.addRibbonIcon('lock', 'Password Protect', (evt: MouseEvent) => {
+            // Open a modal when the icon is clicked
+            new PasswordModal(this.app, (password) => {
+                new Notice(`Password entered: ${password}`);
+                // Add encryption logic here
+            }).open();
+        });
 
-	async onload() {
-		await this.loadSettings();
+        // Perform additional things with the ribbon
+        ribbonIconEl.addClass('password-protect-ribbon-class');
+    }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    onunload() {
+        console.log('Unloading Password Protect Plugin');
+    }
 }
